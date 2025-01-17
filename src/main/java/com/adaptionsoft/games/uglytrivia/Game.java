97bf8bb8ac7e019b4ee3517f7cd5abaafbcca906
carlocarlen <cmaffi@cross-systems.com>
2025-01-17 08:35:09 +0100

    Analysis

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -5,22 +5,33 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Game {
-	
+
+	// 6 methods
 	private final PrintStream printStream;
-	
+
+	// 5 methods
     ArrayList<String> players = new ArrayList<>();
+	// 3 methods: add, roll, currentCategory
     int[] places = new int[6];
+	// 3 methods: add, wasCorrectlyAnswered, didPlayerWin
     int[] purses  = new int[6];
+	// 4 methods
     boolean[] inPenaltyBox  = new boolean[6];
-    
+
+	// 2 methods: constructor and askQuestion
     LinkedList<String> popQuestions = new LinkedList<>();
+	// 2 methods: constructor and askQuestion
 	LinkedList<String> scienceQuestions = new LinkedList<>();
+	// 2 methods: constructor and askQuestion
 	LinkedList<String> sportsQuestions = new LinkedList<>();
+	// 2 methods: constructor and askQuestion
 	LinkedList<String> rockQuestions = new LinkedList<>();
-    
+
+	// 5 methods
     int currentPlayer = 0;
+	// 2 methods: roll, wasCorrectlyAnswered
     boolean isGettingOutOfPenaltyBox;
-    
+
     public  Game(PrintStream printStream){
         this.printStream = printStream;
         for (int i = 0; i < 50; i++) {
@@ -31,14 +42,17 @@ public class Game {
     	}
     }
 
+	// 0 references
 	public String createRockQuestion(int index){
 		return "Rock Question " + index;
 	}
-	
+
+	// 1 method
 	public boolean isPlayable() {
 		return (howManyPlayers() >= 2);
 	}
 
+	// 5 fields + howManyPlayers
 	public boolean add(String playerName) {
 		
 		
@@ -51,11 +65,13 @@ public class Game {
 	    printStream.println("They are player number " + players.size());
 		return true;
 	}
-	
+
+	// 1 field
 	public int howManyPlayers() {
 		return players.size();
 	}
 
+	// A lot
 	public void roll(int roll) {
 		printStream.println(players.get(currentPlayer) + " is the current player");
 		printStream.println("They have rolled a " + roll);
@@ -92,6 +108,7 @@ public class Game {
 		
 	}
 
+	// 5 fields questions and printStream + currentCategory()
 	private void askQuestion() {
 		if (currentCategory().equals("Pop"))
 			printStream.println(popQuestions.removeFirst());
@@ -103,7 +120,7 @@ public class Game {
 			printStream.println(rockQuestions.removeFirst());		
 	}
 	
-	
+	// 2 fields
 	private String currentCategory() {
 		if (places[currentPlayer] == 0) return "Pop";
 		if (places[currentPlayer] == 4) return "Pop";
@@ -117,6 +134,7 @@ public class Game {
 		return "Rock";
 	}
 
+	// A lot
 	public boolean wasCorrectlyAnswered() {
 		if (inPenaltyBox[currentPlayer]){
 			if (isGettingOutOfPenaltyBox) {
@@ -156,7 +174,8 @@ public class Game {
 			return winner;
 		}
 	}
-	
+
+	// 4 fields
 	public boolean wrongAnswer(){
 		printStream.println("Question was incorrectly answered");
 		printStream.println(players.get(currentPlayer)+ " was sent to the penalty box");
@@ -166,8 +185,8 @@ public class Game {
 		if (currentPlayer == players.size()) currentPlayer = 0;
 		return true;
 	}
-
-
+	
+	// 2 fields
 	private boolean didPlayerWin() {
 		return !(purses[currentPlayer] == 6);
 	}
